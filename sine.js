5fcd75fd4001d603593d2f18494cff6846bbbe8a
Dany Cervantes <danycervantesq@gmail.com>
2022-12-20 18:54:58 -0500

    Update sine.js

--- sine.js
+++ sine.js
@@ -8,47 +8,51 @@ canvas.height = screenHeight;
 canvas.width = screenWidth;
 c = canvas.getContext("2d");
 
-const sliderColor = document.getElementById("color");
-const colorRange = document.getElementById("color-range");
-const sliderSat = document.getElementById("saturation");
-const satRange = document.getElementById("saturation-range");
-const sliderLight = document.getElementById("lightness");
-const lightRange = document.getElementById("light-range");
-
+//for interval use
 let allow = true;
 let off;
 let time = 0;
 
+//slider color controls
+const sliderColor = document.getElementById("color");
+const colorRange = document.getElementById("color-range");
+const sliderLight = document.getElementById("lightness");
+const lightRange = document.getElementById("light-range");
+const sliderSat = document.getElementById("saturation");
+const satRange = document.getElementById("saturation-range");
 
-//Returns a random number within a chosen range
-function randomRange(min,max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-//Math.floor() rounds down to the nearest whole number  e.i. 10 = 0 - 9  
-//Math.random() returns a random decimal between 0 - 0.99
-}
-
-
-let wave = {
-    y: screenHeight / 2,
-    length: randomRange(-0.01, 0.01),
-    amplitude: randomRange(-250, 250),
-    frequency: randomRange(-0.01, 1)
-}
-
-let increment = wave.frequency;
 let color = {
     h: sliderColor.value,
     l: sliderLight,
     s: sliderSat.value
 }
 
+//slider wave controls
+const amp = document.getElementById("amplitude");
+const ampRange = document.getElementById("amp-range");
+const freq = document.getElementById("frequency");
+const freqRange = document.getElementById("freq-range");
+const waveLength = document.getElementById("wave-length");
+const lengthRange = document.getElementById("length-range");
+
+let wave = {
+    y: screenHeight / 2,
+    length: waveLength / 10000, 
+    amplitude: amp, 
+    frequency: freq.value / 10000
+}
+
+let increment = wave.frequency;
+
+
 function animate() {
 
     requestAnimationFrame(animate);
 
-    c.fillStyle = `rgba(0, 0, 0, 0.01)`;
+    c.fillStyle = `rgba(0, 0, 0, 0.02)`;
     c.fillRect(0,0,screenWidth,screenHeight);
     
+    //object
     c.beginPath();
     c.moveTo(0, screenHeight / 2);
     for(i = 0; i < screenWidth; i++) {
@@ -58,26 +62,59 @@ function animate() {
     c.stroke();
     c.closePath();
     
-    
+
     increment += wave.frequency;
     wave.y = screenHeight / 2,
     
+    //update color values
     color.h = sliderColor.value;
     colorRange.innerHTML = sliderColor.value;
     color.l = sliderLight.value;
     lightRange.innerHTML = sliderLight.value;
     color.s = sliderSat.value;
     satRange.innerHTML = sliderSat.value;
-    
+
+    //update wave values
+    wave.length = waveLength.value / 10000;
+    lengthRange.innerHTML = waveLength.value / 10000;
+    wave.amplitude = amp.value;
+    ampRange.innerHTML = amp.value;
+    wave.frequency = freq.value / 10000;
+    freqRange.innerHTML = freq.value / 10000; 
 }
 
 
 canvas.addEventListener("click", function() {
-    
+
     portfolio.style.visibility = "visible";
     slider.style.visibility = "visible";
 
-    time = 10000; //10 seconds, resets on click
+    time = 15000; //10 seconds, resets on click
+    
+    if(allow) {
+
+        allow = false; //prevents multiple intervals
+
+        off = setInterval(() => {
+            time -= 1000;
+        
+            if(time <= 0) {
+                portfolio.style.visibility = "hidden";
+                slider.style.visibility = "hidden";
+                clearInterval(off);
+                allow = true;
+            }
+        }, 1000);
+    }
+});
+
+
+canvas.addEventListener("mousemove", function() {
+
+    portfolio.style.visibility = "visible";
+    slider.style.visibility = "visible";
+
+    time = 15000; //10 seconds, resets on click
     
     if(allow) {
 
